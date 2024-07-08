6f9752b710da9f2fef6b1b4629dccc5e1899bbd8
isurubandara1 <isurumbandara@gmail.com>
2024-07-08 22:34:34 +0530

    server.js contant add

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,12 @@ const app = express();
 
 app.use(cors());
 
-
+mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password:"W7301@jqir#",
+    database: "StdMgtSystem"
+})
 
 app.listen(8081, ()=>{
     console.log("listening");
