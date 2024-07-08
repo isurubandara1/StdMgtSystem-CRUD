8a52cb69a44bc7cf734597ce5608e435948f86b9
isurubandara1 <isurumbandara@gmail.com>
2024-07-08 22:24:39 +0530

    server.js create

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+    const cros = require("cors");
+    const mysql = require("mysql");
 const app = express();
+
+app.use(cors());
 
 app.listen(8081, ()=>{
     console.log("listening");
